4cbb901b88ba29e1d88d95b681212c608ff0a794
Your Name <you@example.com>
2020-05-13 22:43:23 +0530

    First Code

---
+++ Factorial.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std ;
+
+int main ( )
+{
+	int x , fact ;
+	
+	cout << "Enter a number to calculate its factorial : " << endl ;
+	cin >> x ;
+	
+	fact = x ;
+	while ( x > 1 )
+	{
+		x -- ;
+		fact = fact * x ;
+	}
+	
+	cout << "Factorial Value : " << fact ;
+	
+	return 0 ;
+}
